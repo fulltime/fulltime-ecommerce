50e66151f8af654493d80b207b9f591a681d408d
Patrick König <p.koenig@fulltime-ecommerce.de>
2024-03-07 16:37:21 +0100

    test

---
+++ app/fonts.tsx
@@ -0,0 +1,5 @@
+import { Jost, Poppins } from 'next/font/google'
+
+export const jost = Jost({ subsets: ['latin'], display: 'swap' })
+
+export const poppins = Poppins({ subsets: ['latin'], weight: ["400"], display: 'swap' })
