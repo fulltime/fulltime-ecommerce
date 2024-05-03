b1d0567a5e616cf4db4b2d7d5bdade4562660a87
Patrick König <p.koenig@fulltime-ecommerce.de>
2024-05-03 16:38:24 +0200

    fix

--- app/[[...page]]/page.tsx
+++ app/[[...page]]/page.tsx
@@ -27,7 +27,7 @@ export default async function Page(props: PageProps) {
 
   return (
     <>
-      <RenderBuilderContent content={content} />
+      <RenderBuilderContent content={content} model={type} />
     </>
   );
 }
--- components/builder.tsx
+++ components/builder.tsx
@@ -7,19 +7,20 @@ import '@builder.io/widgets';
 
 interface BuilderPageProps { 
   content?: BuilderContent;
+  model: string;
 }
 
 // Replace with your Public API Key
 builder.init(process.env.NEXT_PUBLIC_BUILDER_API_KEY!);
 
-export function RenderBuilderContent({ content }: BuilderPageProps) { 
+export function RenderBuilderContent({ content, model }: BuilderPageProps) { 
   // Call the useIsPreviewing hook to determine if 
   // the page is being previewed in Builder
   const isPreviewing = useIsPreviewing(); 
   // If `content` has a value or the page is being previewed in Builder,
   // render the BuilderComponent with the specified content and model props.
   if (content || isPreviewing) {
-    return <BuilderComponent content={content} model="page" />;
+    return <BuilderComponent content={content} model={model} />;
   }
   // If the `content` is falsy and the page is 
   // not being previewed in Builder, render the 
